18fd30c04bb3b6c448405a882fa7c3e182b2df8e
Andrew Fecheyr <andrew@bedesign.be>
2013-06-03 09:19:21 +0200

    Manually create user and privilege

--- recipes/database.rb
+++ recipes/database.rb
@@ -7,19 +7,35 @@ include_recipe "postgresql::ruby"
 
 postgresql_connection_info = {:host => "localhost",
                               :port => node['postgresql']['config']['port'],
-                              :username => 'postgres',
+                              :user => 'postgres',
                               :password => node['postgresql']['password']['postgres']}
 
-# Create database user
-postgresql_database_user(node['coinz']['db']['user']) do
-  connection postgresql_connection_info
-  password node['coinz']['db']['password']
-  action :create
-end
+# # Create database user
+# postgresql_database_user(node['coinz']['db']['user']) do
+#   connection postgresql_connection_info
+#   password node['coinz']['db']['password']
+#   action :create
+# end
 
-# Grant user the right to create new databases
-postgresql_database("GRANT create TO #{node['coinz']['db']['user']}") do
-  connection postgresql_connection_info
-  action :query
-end
+# Create database user and grant priviledges
+# postgresql_database "Creat database user" do
+#   pwd = node['coinz']['db']['password']
+#   sql "CREATE ROLE #{node['coinz']['db']['user']} WITH CREATEDB LOGIN PASSWORD '#{pwd}'"
+#   Chef::Log.error("SQL: #{sql}")
+#   connection postgresql_connection_info
+#   action :query
+# end
 
+# Create database user and grant priviledges
+# Doing it manually because the LWRP's did not work
+ruby_block "Create db user with createdb privilege" do
+  block do
+    PGconn.open(postgresql_connection_info) do |conn|
+      begin
+        result = conn.exec "CREATE ROLE #{node['coinz']['db']['user']} WITH CREATEDB LOGIN PASSWORD '#{node['coinz']['db']['password']}'"
+      rescue PG::Error => e
+        Chef::Log.warn("Error creating db user: #{e.message.chomp}")
+      end
+    end
+  end
+end
