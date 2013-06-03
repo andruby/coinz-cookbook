1c840e1d60899c531e07bfd083b6b02119ed9841
Andrew Fecheyr <andrew@bedesign.be>
2013-06-03 12:54:06 +0200

    Run bundle install with sudo

--- recipes/development.rb
+++ recipes/development.rb
@@ -2,8 +2,15 @@
 # Cookbook Name:: coinz
 # Recipe:: development
 #
-# Recipe that sets up an Virtual development machine
+# Recipe that sets up a Virtual development machine
 #
+# This recipe should only contain statements that are needed for vagrant/development
+# and NOT needed for production
 
 include_recipe "coinz::requirements"
 include_recipe "coinz::database"
+
+# Run bundle install as sudo in /vagrant
+execute "bundle install" do
+  cwd "/vagrant"
+end
--- recipes/requirements.rb
+++ recipes/requirements.rb
@@ -6,12 +6,14 @@ include_recipe "apt"
 #
 # Ruby 2.0 as system global ruby
 #
+
+# Compile ruby 2.0.0 from source
 include_recipe "ruby_build"
 ruby_build_ruby "2.0.0-p195" do
   prefix_path "/usr/local/"
 end
 
+# Install bundler with the correct ruby 2.0.0 gem binary
 gem_package "bundler" do
   gem_binary "/usr/local/bin/gem"
-  options("--no-ri --no-rdoc")
 end
