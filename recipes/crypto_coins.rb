78d66c0af9a8781bd2d079164b11a2bbbece3ad2
Andrew Fecheyr <andrew@bedesign.be>
2013-06-08 21:59:59 +0200

    Add worldcoin and bbqcoin to he fun

--- recipes/crypto_coins.rb
+++ recipes/crypto_coins.rb
@@ -1,15 +1,33 @@
 
 include_recipe "crypto-coin::default"
 
-# Compile & setup litecoind
 crypto_coin "litecoin" do
   repository    "https://github.com/litecoin-project/litecoin.git"
   revision      "0.6.3"
   port          9333
-  rpcpassword   "nojxxq2rryghg1p0ti7x"
+  rpcport       7001
+  rpcpassword   "npdbkbq9t6x9vt558qr9"
 end
 
-service "litecoind" do
-  provider Chef::Provider::Service::Upstart
-  action :start
+crypto_coin "bbqcoin" do
+  repository    "https://github.com/overware/BBQCoin.git"
+  revision      "b742b74481598e5e3dc104fa6be62ab04cf2b7ff"
+  port          59332
+  rpcport       7002
+  rpcpassword   "5n0g6r5z5w9pazeurqb6"
+end
+
+crypto_coin "worldcoin" do
+  repository    "https://github.com/worldcoinproject/Worldcoin.git"
+  revision      "2d35dc4ba7365a1873e8c0523dbae483058779f8"
+  port          11081
+  rpcport       7003
+  rpcpassword   "mtrvhninryeu53wzmczc"
+end
+
+%w{litecoind bbqcoind worldcoind}.each do |coind|
+  service(coind) do
+    provider Chef::Provider::Service::Upstart
+    action :start
+  end
 end
