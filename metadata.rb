3e8ed8660ac5954e0a465c4a8d774f0fc6331cf0
Andrew Fecheyr <andrew@bedesign.be>
2013-06-09 10:05:06 +0200

    Bump crypto-coin to v0.2.2

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,6 @@ depends "ruby_build", "~> 0.8.0"
 depends "postgresql", "~> 3.0.0"
 depends "apt", "~> 1.10.0"
 depends "database", "~> 1.4.0"
-depends "crypto-coin", "~> 0.2.1"
+depends "crypto-coin", "~> 0.2.2"
 
 supports "ubuntu"
