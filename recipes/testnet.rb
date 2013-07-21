f309b5141709434c637396d3277da6742fed4d0a
Andrew Fecheyr <andrew@bedesign.be>
2013-07-22 01:12:34 +0200

    Recipe with bitcoin and litecoin on testnet

---
+++ recipes/testnet.rb
@@ -0,0 +1,27 @@
+# "role" recipe for coin testnets
+
+include_recipe "crypto-coin::default"
+
+crypto_coin "bitcoin" do
+  repository    "https://github.com/bitcoin/bitcoin.git"
+  revision      "40809aed657502e9de158e2cfe2c659a316f2f90"
+  port          18333
+  rpcport       7001
+  rpcpassword   "fa4rhioylwmmzfff"
+  conf({
+    testnet: 1,
+    gen: 1
+  })
+end
+
+crypto_coin "litecoin" do
+  repository    "https://github.com/litecoin-project/litecoin.git"
+  revision      "bf293d7b6ddebba22482a6ed3730731e481ccc94"
+  port          19333
+  rpcport       7002
+  rpcpassword   "7a4kh3i2m9tfdxf7"
+  conf({
+    testnet: 1,
+    gen: 1
+  })
+end
